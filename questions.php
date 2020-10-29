32998104c371d08c8d86ae3cdf415afbaf652b8f
kaw37 <55502441+kaw37@users.noreply.github.com>
2020-10-29 15:57:07 -0400

    Update questions.php

--- questions.php
+++ questions.php
@@ -2,19 +2,19 @@
 extract($_POST);
 
 if (!$questionName || strlen($questionName)<4)
-    die ("Question Name Missing or too short!");
+    die ("Question name missing or too short!");
 
 if (!$questionBody || strlen($questionName)> 500)
-    die ("Question Body Missing or too long!");
+    die ("Question body missing or too long!");
 
 $skills = explode (",", $questionSkills);
 $length = count($skills);
 if ($length < 2)
-    die("Not enough skills");
+    die("Not enough skills!");
 
 echo "Length: $length <br/>";
 for ($i = 0; $i < $length; $i++) {
     print "$i) $skills[$i] <br/>";
 }
 ?>
-<a href="index.html">Home</a>
+<div><a href="project1index.html">Home</a></div>
